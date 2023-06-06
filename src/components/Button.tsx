e66d141217b8500ffc86527526243ee71e4ae23a
Rodrigo Lemos <rodrigosllemos@gmail.com>
2023-06-06 13:45:03 -0300

    vite version update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,22 @@
 import { Slot } from "@radix-ui/react-slot";
 import clsx from "clsx";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...rest }: ButtonProps) {
   const Comp = asChild ? Slot : "button";
   return (
     <Comp
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded font-semibold font-sans text-black text-base w-full hover:bg-cyan-300 transition-colors duration-200 focus:ring-2 ring-gray-100"
+        "py-3 px-4 bg-cyan-500 rounded font-semibold font-sans text-black text-base w-full hover:bg-cyan-300 transition-colors duration-200 focus:ring-2 ring-gray-100",
+        className
       )}
+      {...rest}
     >
       {children}
     </Comp>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,17 +6,27 @@ export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({ size = "md", children, asChild }: HeadingProps) {
+export function Heading({
+  size = "md",
+  className,
+  children,
+  asChild,
+}: HeadingProps) {
   const Comp = asChild ? Slot : "h2";
   return (
     <Comp
-      className={clsx("text-gray-100 font-bold font-sans", {
-        "text-xl": size === "sm",
-        "text-2xl": size === "md",
-        "text-3xl": size === "lg",
-      })}
+      className={clsx(
+        "text-gray-100 font-bold font-sans",
+        {
+          "text-xl": size === "sm",
+          "text-2xl": size === "md",
+          "text-3xl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Comp>
